db8619c4fa602c527adaf8cbd2cf6969115f1837
HASANsuliman <hassnsuliman50@gmail.com>
2022-07-02 12:12:26 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,216 @@
-import { Component } from '@angular/core';
-
+import { Component, HostBinding, OnInit, AfterViewInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { FormControl } from '@angular/forms';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import * as AOS from 'aos';
+import { MatIconRegistry } from '@angular/material/icon';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'MySite';
+  cols = 3;
+  Testimonialscols2 = 3;
+  projectscols2 = 2;
+  projectsrows = '410px';
+  gaugerows = '';
+  gaugecol = 2;
+  cardhe = 'cardhe';
+  a2port = 'a2port';
+  rescols = '2';
+  resrows = '260px';
+  cardresumehe = 'cardresumehe';
+  rows2 = '340px';
+  Testimonialsrows2='360px'
+  rowscont = '360';
+  rows = '250px';
+  navdivhide = true;
+  menu = false;
+  guagetile = '';
+  toggleControl = new FormControl(false);
+  @HostBinding('class') className = '';
+  gauge = [
+    { id: 1, gaugeValue: 85, gaugeLabel: 'Smart', gaugeAppendText: ' %' },
+    { id: 2, gaugeValue: 95, gaugeLabel: 'Work', gaugeAppendText: ' %' },
+    { id: 3, gaugeValue: 80, gaugeLabel: 'Leader', gaugeAppendText: ' %' },
+    { id: 4, gaugeValue: 90, gaugeLabel: 'Support', gaugeAppendText: ' %' },
+    { id: 4, gaugeValue: 80, gaugeLabel: 'social', gaugeAppendText: ' %' },
+    { id: 4, gaugeValue: 92, gaugeLabel: 'competitor', gaugeAppendText: ' %' },
+  ];
+  intersted = ['BackEnd', 'FrontEnd', 'DataBase'];
+
+  backgroundColor = '#138d8d';
+  fgc = '#00c8c8';
+  thick = 8;
+
+  constructor(
+    private responsive: BreakpointObserver,
+    private overlay: OverlayContainer,
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIcon(
+      'css',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/css.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'git',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/git2.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'lin',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/lin.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'arr',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/arr.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'html',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/html.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'c',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/c.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'ora2',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/ora2.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'sql',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/sql.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'ang',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/ang.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'vue',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/vue.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'actv',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/actv.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'boot',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/boot.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'mat',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/mat.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'face',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/fc1.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'tele',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/telg.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'mail',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/mail.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'call',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/svg/cl.svg')
+    );
+  }
+  ngAfterViewInit(): void {
+    AOS.init();
+  }
+  ngOnInit(): void {
+    this.toggleControl.valueChanges.subscribe((darktheme) => {
+      const darkClassName = 'darktheme';
+      this.className = darktheme ? darkClassName : '';
+      if (darktheme) {
+        this.overlay.getContainerElement().classList.add(darkClassName);
+      } else {
+        this.overlay.getContainerElement().classList.remove(darkClassName);
+      }
+    });
+    this.responsive
+      .observe([
+        Breakpoints.HandsetLandscape,
+        Breakpoints.TabletLandscape,
+        Breakpoints.TabletPortrait,
+        Breakpoints.HandsetPortrait,
+      ])
+      .subscribe((result) => {
+        this.cols = 3;
+        this.rows = '250px';
+        this.Testimonialscols2 = 3;
+        this.projectscols2 = 2;
+        this.cardhe = '';
+        this.a2port = '';
+        this.gaugerows = '200';
+        this.gaugecol = 2;
+        this.projectsrows = '450px';
+        this.rows2 = '370px';
+        this.Testimonialsrows2='390px'
+        this.menu = false;
+        this.navdivhide = true;
+        this.rescols = '2';
+        this.resrows = '320px';
+        this.guagetile = '';
+        const BP = result.breakpoints;
+        if (BP[Breakpoints.TabletPortrait]) {
+          console.log(result + 'TP');
+          this.cols = 1;
+          this.rows2 = '300px';
+          this.Testimonialscols2 = 1;
+          this.projectscols2 = 1;
+          this.gaugecol = 1;
+          this.menu = true;
+          this.a2port = 'a2port';
+          this.navdivhide = false;
+          this.guagetile = 'auto';
+        }
+        if (BP[Breakpoints.TabletLandscape]) {
+          console.log(BP + 'TL');
+          this.Testimonialscols2 = 3;
+          this.projectscols2 = 2;
+          this.projectsrows = '500px';
+          this.rows2 = '450px';
+          this.cols = 2;
+          this.menu = false;
+          this.navdivhide = true;
+        }
+        if (BP[Breakpoints.HandsetPortrait]) {
+          console.log(BP + 'HP');
+          this.rows = '270px';
+          this.cols = 1;
+          this.Testimonialscols2 = 1;
+          this.projectscols2 = 1;
+          this.projectsrows = '590px';
+          this.gaugerows = '300';
+          this.gaugecol = 1;
+          this.resrows = '450';
+          this.cardhe = 'cardhe';
+          this.a2port = 'a2port';
+          this.rows2 = '500px';
+          this.menu = true;
+          this.navdivhide = false;
+          this.guagetile = 'auto';
+        }
+        if (BP[Breakpoints.HandsetLandscape]) {
+          console.log(BP + 'HL');
+          this.a2port = 'a2hand';
+          this.guagetile = 'auto';
+          this.gaugerows = '300';
+          this.resrows = '330';
+          this.cols = 1;
+          this.Testimonialscols2 = 1;
+          this.projectscols2 = 1;
+          this.gaugecol = 1;
+          this.menu = true;
+          this.navdivhide = false;
+        }
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { NgxGaugeModule } from 'ngx-gauge';
+import {MatSelectModule} from '@angular/material/select';
 
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    MatGridListModule,
+    MatListModule,
+    MatMenuModule,
+    MatIconModule,
+    HttpClientModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    NgxGaugeModule,
+    MatSelectModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide:LocationStrategy, useClass:HashLocationStrategy}
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
